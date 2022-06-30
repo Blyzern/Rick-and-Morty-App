6d07c59f8d6eb439f2754defc82d0a19229baa62
Salvo <blyzer.game@gmail.com>
2022-06-30 19:41:16 +0200

    feat(develop): setting absolute imports in tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Counter } from '@counter/Counter'; // <-- non funziona
+//import { Counter } from './features/counter/Counter';  <-- funziona
 import './App.css';
 
 function App() {
