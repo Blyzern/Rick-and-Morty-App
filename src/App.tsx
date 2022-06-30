184360ca25b7a74ac4e5c9c5b874260da7c7c6bf
Salvo <blyzer.game@gmail.com>
2022-06-30 20:30:54 +0200

    feat(general): fixing import bug.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from '@counter/Counter'; // <-- non funziona
-//import { Counter } from './features/counter/Counter';  <-- funziona
+import { Counter } from 'src/features/counter/Counter';
 import './App.css';
 
 function App() {
