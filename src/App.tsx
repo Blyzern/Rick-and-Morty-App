865be1ba92b9b29810398e2846bec00f33c3f30c
Salvo <blyzer.game@gmail.com>
2022-07-07 19:50:15 +0200

    feat(eslint): solved many eslint error and setted all configurations needed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 const App: FC = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
